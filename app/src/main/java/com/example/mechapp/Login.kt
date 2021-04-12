a9fad98c74064298d00682dd103e3d5d12d3cba1
Nickolai <nipet18@student.sdu.dk>
2021-04-12 12:51:01 +0100

    Main pages made

--- app/src/main/java/com/example/mechapp/Login.kt
+++ app/src/main/java/com/example/mechapp/Login.kt
@@ -2,10 +2,13 @@ package com.example.mechapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+
+        Button button = findViewById(R.id.)
     }
 }
