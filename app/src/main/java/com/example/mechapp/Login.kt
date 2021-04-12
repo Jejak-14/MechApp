d2453be0c5ac1d0e2e38696c92f8f1040f7fd1cc
Nickolai <nipet18@student.sdu.dk>
2021-04-12 14:37:56 +0100

    Beginning to add logic

--- app/src/main/java/com/example/mechapp/Login.kt
+++ app/src/main/java/com/example/mechapp/Login.kt
@@ -1,7 +1,10 @@
 package com.example.mechapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import android.widget.Button
 
 class Login : AppCompatActivity() {
@@ -9,6 +12,24 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        Button button = findViewById(R.id.)
+        val button = findViewById<Button>(R.id.logInBtn)
+
+        button.setOnClickListener {
+            val intent = Intent(this, actictivity_Status::class.java)
+
+            startActivity(intent)
+        }
+        /*Buttons*/
+        fun nextButton(view: View){
+            val button = findViewById<Button>(R.id.logInBtn)
+
+            button.setOnClickListener {
+                val intent = Intent
+
+                startActivity(intent)
+
+            }
+        }
     }
 }
+
